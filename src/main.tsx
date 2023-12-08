8f7abf44d07a32b32cd623c403e5ef8f7d1c83fb
Pablo Ferrari <pabloferraricaliari@gmail.com>
2023-12-08 09:12:00 -0300

    makes the header component global

--- src/main.tsx
+++ src/main.tsx
@@ -7,10 +7,13 @@ import "./index.css";
 import ProductsProvider from "./providers/ProductsProvider.tsx";
 import { routers } from "./http/routes/index.tsx";
 import { RouterProvider } from "react-router-dom";
+import Header from "./components/Header/index.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ProductsProvider>
+      <Header />
+
       <RouterProvider router={routers} />
     </ProductsProvider>
   </React.StrictMode>
